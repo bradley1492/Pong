bd26ecf6d88b2f2e7eaa487d4c915e1f5e585e1b
bradley1492 <bradleytochristopher@gmail.com>
2020-12-20 19:07:21 +0100

    Built Purple Window

--- pong.js
+++ pong.js
@@ -1,7 +1,31 @@
+/*
+class Vec
+{
+	constructor(x = 0, y = 0)
+	{
+		this.x = x;
+		this.y = y;
+	} 
+}
+
+class Rect
+{
+	constructor(w, h)
+	{
+		this.pos = new Vec;
+		this.size = new Vec(w, h);
+	}
+}
+
+class Ball extends Rect
+{
+	constructor
+}
+*/
 const canvas = document.getElementById('pong');
 //const context = canvas.getContext(
 //const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
 context.fillStyle = 'blue';
-context.fillRect(10, 10, 100, 100);
+context.fillRect(0, 0, canvas.width, canvas.height);
