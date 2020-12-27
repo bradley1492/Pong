873f4f851f6fedab40a0edc72662254506df30d9
bradley1492 <bradleytochristopher@gmail.com>
2020-12-27 18:10:17 +0100

    Continued Tutorial

--- pong.js
+++ pong.js
@@ -1,5 +1,4 @@
-/*
-class Vec
+ class Vec
 {
 	constructor(x = 0, y = 0)
 	{
@@ -8,24 +7,41 @@ class Vec
 	} 
 }
 
-class Rect
-{
-	constructor(w, h)
+class Rect {
+	constructor(w, h) 
 	{
 		this.pos = new Vec;
 		this.size = new Vec(w, h);
 	}
 }
 
-class Ball extends Rect
-{
-	constructor
+class Ball extends Rect {
+	constructor()
+	{	
+		super(10, 10);
+		this.vel = new Vec;
+	}
 }
-*/
+
 const canvas = document.getElementById('pong');
 //const context = canvas.getContext(
 //const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-context.fillStyle = 'blue';
+const ball = new Ball;
+ball.pos.x = 100;
+ball.pos.y = 50;
+console.log(ball); 
+
+function update(dt) {
+	ball.pos.x += ball.cel.x * dt;
+	ball.pos.y += ball.vel.y * dt;
+
+	context.fillStyle = 'black';
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+context.fillStyle = 'white';
+context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+}
+
+ 
